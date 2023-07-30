9edb1d4daa2b58075b44f2fdabff21d035da53d1
Ckott <diegoangeles130703@gmail.com>
2023-07-30 04:03:27 -0500

    reactform

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter ,Routes, Route} from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hola Mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="/login" element={<h1><LoginPage/></h1>}/>
+        <Route path="/register" element={<h1><RegisterPage/></h1>}/>
+        <Route path="/tasks" element={<h1>Home Page</h1>}/>
+        <Route path="/add-task" element={<h1>New task</h1>}/>
+        <Route path="/task/:id" element={<h1>update Tasks</h1>}/>
+        <Route path="/profile" element={<h1>Profile</h1>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,23 @@
+import {useForm} from "react-hook-form"
+
+function RegisterPage() {
+
+    const {register, handleSubmit} = useForm()
+
+    return (
+        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+            <form action="" onSubmit={handleSubmit(values => {
+                console.log(values)
+            })}>
+                <input type="text" {...register("username", {required : true})} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Username"/>
+                <input type="email" {...register("email", {required : true})} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="email@email.com"/>
+                <input type="password" {...register("password", {required : true})} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Password"/>
+                <button type="submit">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default RegisterPage;
